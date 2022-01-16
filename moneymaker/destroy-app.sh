53ced85d2357fdf890cfac5ffd500af2b1d86a1a
JayDamon <Jay.n.damon@gamil.com>
2022-01-16 10:02:54 -0500

    refactor: Modify secret location

--- moneymaker/destroy-app.sh
+++ moneymaker/destroy-app.sh
@@ -12,7 +12,7 @@ kubectl delete -f app-config.yml
 
 kubectl delete -f config-map.yml
 
-kubectl delete -f ../../secrets/moneymaker-secrets-jd-mm.yml
+kubectl delete -f ../../secrets/moneymaker-app-secrets.yml -n jd-mm
 
 kubectl delete -f ../../secrets/canister-registry-secret.yml -n jd-mm
 
